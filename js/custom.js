548cfdff7fb70ed05237300365b4c8b2522d2533
ruchitamalviya <88784758+ruchitamalviya@users.noreply.github.com>
2021-09-20 20:28:45 +0530

    Add files via upload
    changeadded

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,24 @@
 jQuery(document).ready(function() {
-        jQuery('#search').keyup(function() {
-            jQuery('#result').html('');
-            var searchField = jQuery('#search').val();
-            var expression = new RegExp(searchField, "i");
-            jQuery.getJSON('words_dictionary.json', function(data) {
-                jQuery.each(data, function(val) {
-                    if (val.search(expression) != -1) {
-                      jQuery('#result').append('<li class="list-group-item link-class">' + val + '</li>');
-                    }
-                });
+    jQuery.ajaxSetup({
+        cache: false
+    });
+    jQuery('#search').keyup(function() {
+        jQuery('#result').html('');
+        var searchField = jQuery('#search').val();
+        var expression = new RegExp(searchField, "i");
+        jQuery.getJSON('words_dictionary.json', function(data) {
+            jQuery.each(data, function(val) {
+                //let length=val.length;
+                //console.log(length);
+                if (val.search(expression) === 0) {
+                    jQuery('#result').append('<li class="list-group-item link-class">' + val + '</li>');
+                }
             });
         });
-        jQuery('#result').on('click', 'li', function() {
-            var click_text = jQuery(this).text().split('|');
-            jQuery('#search').val(jQuery.trim(click_text[0]));
-            jQuery("#result").html('');
-        });
     });
+    jQuery('#result').on('click', 'li', function() {
+        var click_text = jQuery(this).text().split('|');
+        jQuery('#search').val(jQuery.trim(click_text[0]));
+        jQuery("#result").html('');
+    });
+});
